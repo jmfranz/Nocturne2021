355942b15d2f1ba3730b3dfaf91a343bde2ecfef
Juliano Franz <juliano.franz@dal.ca>
2021-09-17 17:36:49 -0300

    Update FollowMainCamera.cs

--- Assets/Nocture2021/Scripts/Network/FollowMainCamera.cs
+++ Assets/Nocture2021/Scripts/Network/FollowMainCamera.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 
 public class FollowMainCamera : MonoBehaviour
 {
     void LateUpdate()
     {
-        transform.position = Camera.main.GetComponent<Transform>().position;
-        transform.rotation = Camera.main.GetComponent<Transform>().rotation;
+        if (GetComponent<PhotonView>().IsMine)
+        {
+            transform.position = Camera.main.GetComponent<Transform>().position;
+            transform.rotation = Camera.main.GetComponent<Transform>().rotation;
+        }
     }
 }
