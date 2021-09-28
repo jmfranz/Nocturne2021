136b7efc88bdeda4c27cc59bd3b14f0b82b9ef4d
Hubert (Sathaporn) Hu <hu.hubert@live.ca>
2021-09-28 12:22:30 -0300

    Made the sure the camera can only log every 0.1 second.

--- Assets/Scripts/CameraPositionLogger.cs
+++ Assets/Scripts/CameraPositionLogger.cs
@@ -16,6 +16,7 @@ public class CameraPositionLogger : MonoBehaviour
     public GameObject parentAnchor; // The anchroing object for the camera.
 
     bool logPathPrinted = false;
+    float timeElapsed = 0;
 
     private string sceneName;
 
@@ -43,22 +44,29 @@ public class CameraPositionLogger : MonoBehaviour
             logPathPrinted = true;
         }
 
-        StreamWriter sw = new StreamWriter(path, true);
-        DateTime now = DateTime.Now;
-        string date = now.ToString("yyyy-MM-dd");
+        if (timeElapsed >= 0.1f)
+        {
+            timeElapsed = 0;
 
-        Vector3 pos = camera.transform.InverseTransformPoint(parentAnchor.transform.position);
-        Quaternion rot = camera.transform.rotation;
+            StreamWriter sw = new StreamWriter(path, true);
+            DateTime now = DateTime.Now;
+            string date = now.ToString("yyyy-MM-dd");
+
+            Vector3 pos = camera.transform.InverseTransformPoint(parentAnchor.transform.position);
+            Quaternion rot = camera.transform.rotation;
 
 
 
-        string cam_info = string.Format("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13}", 
-            date, now.Hour, now.Minute, now.Second, now.Millisecond, 
-            pos.x, pos.y, pos.z, 
-            rot.x, rot.y, rot.z, rot.w, 
-            sceneName,
-            PhotonNetwork.IsMasterClient);
-        sw.WriteLine(cam_info);
-        sw.Close();
+            string cam_info = string.Format("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13}",
+                date, now.Hour, now.Minute, now.Second, now.Millisecond,
+                pos.x, pos.y, pos.z,
+                rot.x, rot.y, rot.z, rot.w,
+                sceneName,
+                PhotonNetwork.IsMasterClient);
+            sw.WriteLine(cam_info);
+            sw.Close();
+        }
+
+        timeElapsed += Time.deltaTime;
     }
 }
