062703c6df2c09f4f9873a624caddcf82d880109
Juliano Franz <juliano.franz@dal.ca>
2021-10-05 16:28:07 -0300

    Added 15 minute automatic voice recording
    Sorry for the single commit. BSOD f** my local

---
+++ Assets/Nocture2021/Scripts/MicrophoneAccess.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+#if WINDOWS_UWP
+using Windows.Storage;
+#endif
+
+
+//For now, this is just so we can hear us back
+[RequireComponent(typeof(AudioSource))]
+public class MicrophoneAccess : MonoBehaviour
+{
+    private AudioClip mic;
+
+    private const int RecordTimeInMinutes = 15;
+    public void Start()
+    {
+        var audioSource = GetComponent<AudioSource>();
+        mic = Microphone.Start(Microphone.devices[0], true, 60*RecordTimeInMinutes, 44100);
+        audioSource.clip = mic;
+        audioSource.loop = true;
+        audioSource.Play();
+    }
+
+#if UNITY_EDITOR
+    public void OnGUI()
+    {
+        if (GUILayout.Button("Save me"))
+        {
+            saveAudioFile("rec");
+        }
+    }
+#endif
+
+
+    public void saveAudioFile(string fileName)
+    {
+        
+        var audioSource = GetComponent<AudioSource>();
+        float[] samples = new float[audioSource.clip.samples * audioSource.clip.channels];
+
+
+        SavWav sav = new SavWav();
+        
+        string path = Application.persistentDataPath;
+
+#if WINDOWS_UWP
+        StorageFolder storageFolder = ApplicationData.Current.LocalFolder;
+        path = storageFolder.Path.Replace('\\', '/') + "/";
+#endif
+        string filePath = Path.Combine(path, fileName);
+
+        sav.Save(filePath, mic);
+    }
+}
