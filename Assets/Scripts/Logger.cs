2d93211bfe48e1c36e0b5dcce4ff939121483a96
Hubert (Sathaporn) Hu <hu.hubert@live.ca>
2021-10-06 13:27:58 -0300

    Log saving files separately and remove audio code.

--- Assets/Scripts/Logger.cs
+++ Assets/Scripts/Logger.cs
@@ -73,21 +73,7 @@ public class Logger : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        /*audioClip = Microphone.Start(Microphone.devices[0], false, MaxAudioTime, 44100);
 
-        if (!logFileSetUp)
-        {
-            AddHeaderRequest("audio-starttime.csv", "Date", "Hour", "Minute", "Second", "Milisecond", "AudioStarted", "Scene");
-            logFileSetUp = true;
-        }
-
-        DateTime now = DateTime.Now;
-        string date = now.ToString("yyyy-MM-dd");
-
-        bool audioClipStarted = (audioClip != null);
-
-        WriteRequest("audio-starttime.csv", date, now.Hour, now.Minute, now.Second, now.Millisecond, audioClipStarted,
-            SceneManager.GetActiveScene().name);*/
     }
 
     // Update is called once per frame
@@ -101,7 +87,9 @@ public class Logger : MonoBehaviour
 
             foreach (var k in BufferKeys)
             {
-                string path = Path.Combine(Application.persistentDataPath, k);
+                string idk = UserID.ToString() + "-" + k;
+
+                string path = Path.Combine(Application.persistentDataPath, idk);
 
 
                 if (!File.Exists(path))
@@ -131,18 +119,6 @@ public class Logger : MonoBehaviour
 
         elapsedTime += Time.deltaTime;
 
-        /*if (!Microphone.IsRecording(Microphone.devices[0]))
-            SaveAudio();*/
     }
 
-    private void OnDestroy()
-    {
-        //Microphone.End(Microphone.devices[0]);
-        //SaveAudio();
-    }
-
-    void SaveAudio()
-    {
-        //SavWav.Save(UserID.ToString() + ".wav", audioClip);
-    }
 }
