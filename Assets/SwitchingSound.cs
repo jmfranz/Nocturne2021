8a04b1a9a3d61327e9a8464751cbf31436a49912
Rowland-dal <rw296806@dal.ca>
2021-10-09 13:35:01 -0300

    Sound Effect implemented
    Implemented the sound effect for the cognitive and the real world.

---
+++ Assets/SwitchingSound.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchingSound : MonoBehaviour
+{
+    private AudioSource switchingSoundSource;
+    public AudioClip cognitiveWorldSound;
+    public AudioClip realWorldSound;
+
+    void Awake()
+    {
+
+    }
+
+    void Start()
+    {
+        switchingSoundSource = GetComponent<AudioSource>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            PlayRealWorldSound();
+        }
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            CognitiveRealWorldSound();
+        }
+    }
+
+    public void PlayRealWorldSound()
+    {
+        switchingSoundSource.PlayOneShot(realWorldSound);
+    }
+
+    public void CognitiveRealWorldSound()
+    {
+        switchingSoundSource.PlayOneShot(cognitiveWorldSound);
+    }
+}
