f5c1c76f96c8bf234132640c37615cb8282870ca
Shannon Frederick <shannon_l_frederick@hotmail.com>
2021-10-13 17:30:06 -0300

    try reset

--- Assets/Scripts/Nocturne2021/ConversationNetworking.cs
+++ Assets/Scripts/Nocturne2021/ConversationNetworking.cs
@@ -12,39 +12,53 @@ public class ConversationNetworking : MonoBehaviour, IPunObservable
 
     public int counter;
 
+    public bool reset;
+
 
     // Start is called before the first frame update
     void Start()
     {
         currentConversation = GetComponent<ConversationNode>();
         counter = 0;
+        reset = true;
     }
 
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
-        if (stream.IsWriting)
+        if (reset)
         {
-            stream.SendNext(currentConversation.transform.childCount);
-            for (int i = 0; i < currentConversation.transform.childCount; i++)
+            reset = false;
+            if (stream.IsWriting)
             {
-                stream.SendNext(currentConversation.transform.GetChild(i).name);
+                stream.SendNext(currentConversation.transform.childCount);
+                for (int i = 0; i < currentConversation.transform.childCount; i++)
+                {
+                    stream.SendNext(currentConversation.transform.GetChild(i).name);
+                }
+
             }
-
-        }
-        else
-        {
-            int count = (int)stream.ReceiveNext();
-
-            for (int i = 0; i < count; i++)
+            else
             {
-                string newName = (string)stream.ReceiveNext();
-                Debug.Log(newName);
-                GameObject thisAvatar = GameObject.Find(newName);
-                thisAvatar.GetComponent<AvatarController>().ActiveNode?.RemoveAvatar(currentConversation.gameObject);
-                currentConversation.AddAvatar(thisAvatar);
-                thisAvatar.GetComponent<AvatarController>().ActiveNode = currentConversation;
-            }
+                int count = (int)stream.ReceiveNext();
 
+                for (int i = 0; i < count; i++)
+                {
+                    string newName = (string)stream.ReceiveNext();
+                    Debug.Log(newName);
+                    GameObject thisAvatar = GameObject.Find(newName);
+                    thisAvatar.GetComponent<AvatarController>().ActiveNode?.RemoveAvatar(currentConversation.gameObject);
+                    currentConversation.AddAvatar(thisAvatar);
+                    thisAvatar.GetComponent<AvatarController>().ActiveNode = currentConversation;
+                }
+
+            }
+            StartCoroutine(Wait());
         }
+    }
+
+    public IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(15);
+        reset = true;
     }
 }
